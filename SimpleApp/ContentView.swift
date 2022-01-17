8cc95952518db4c57fa1f4a2a3e2daeb463deb57
rhiannonobrien <rhiannon.obrien@greenwoodcollege.com>
2022-01-17 14:24:27 -0500

    Initial Commit

---
+++ SimpleApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleApp
+//
+//  Created by Rhiannon O'Brien on 2022-01-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleApp/SimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppApp.swift
+//  SimpleApp
+//
+//  Created by Rhiannon O'Brien on 2022-01-17.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
